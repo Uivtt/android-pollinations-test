d1c12d354717c90cf8d2f0761796a63c44e70534
Uivtt <yizutt@qq.com>
2026-09-02 14:05:55 +0800

    Update app/src/main/java/com/test/app/MainActivity.java

---
+++ app/src/main/java/com/test/app/MainActivity.java
@@ -0,0 +1,8 @@
+package com.test.app;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
